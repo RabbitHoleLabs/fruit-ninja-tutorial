ee4f383e683633a7b4fe9bf092326dea8d24cc4a
Vincent Chen <vincentschen@yahoo.com>
2017-02-28 21:41:07 -0800

    Add functional cutting mechanism

---
+++ Assets/SwordCutter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class SwordCutter : MonoBehaviour {
+
+	public Material capMaterial;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+            pieces[1].AddComponent<Rigidbody>();
+
+        Destroy(pieces[1], 1);
+    }
+
+}
